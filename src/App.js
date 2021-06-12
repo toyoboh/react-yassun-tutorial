0e446b7d5e10fb82e91ba5307b288acb56256a14
toyoboh <st.0220@softbank.ne.jp>
2021-06-12 19:19:10 +0900

    youtube3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import React from "react";
 import { List } from "./List";
 
-function App() {
-  const [description, setDescription] = useState("クリック前の表示");
-
-  const changeDescription = () => {
-    setDescription("クリック後の表示");
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: "クリック前の表示"}
   }
 
-  return (
-    <div>
-      { description }
-      <List title="取り扱い言語一覧" />
-      <button onClick={ () => changeDescription() }>ボタン</button>
-    </div>
-  );
+  changeDescription() {
+    this.setState({ description: "クリック後の表示"});
+  }
+
+  render() {
+    const { description } = this.state;
+    return(
+      <div>
+        { description }
+        <List title="取り扱い言語一覧" />
+        <button onClick={ () => this.changeDescription() }>ボタン</button>
+      </div>
+    )
+  }
 }
 
 export default App;
