14ecd9ecda35d680e9f3f45d8fc8e9d7a0b49761
toyoboh <st.0220@softbank.ne.jp>
2021-06-13 01:31:33 +0900

    youtube6

--- src/List.js
+++ src/List.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 export const List = ({ langs }) => {
+
+    useEffect(() => {
+        console.log("List.js:useEffect");
+
+        return () => {
+            console.log("List.js:useEffect:unmounting");
+        }
+    })
+    
     return(
         <div>
             {
